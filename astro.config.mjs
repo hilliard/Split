db18bb4fdd505bd713535ab7b8f1e28a4f3496ea
hilliard <hilliards@gmail.com>
2026-04-26 16:28:31 -0600

    fix: disable checkOrigin for reverse proxy compatibility

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://split.goldtending.com',
+  security: {
+    checkOrigin: false
+  },
   output: 'server',
   adapter: node({
     mode: 'standalone',
